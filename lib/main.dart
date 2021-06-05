4c2da1b2e9edf840ee268ac0f8d2d49244d3c7ab
Josh L. Minga <joshlminga@gmail.com>
2021-06-05 13:42:45 +0300

    Question Index

--- lib/main.dart
+++ lib/main.dart
@@ -14,24 +14,43 @@ void main() => runApp(MyApp());
  * - Main class to build and load our Flutter Widgets
  */
 class MyApp extends StatelessWidget {
+  //Question Answered Method
+  void questionAnswered() {
+    print('Question Answer');
+  }
+
   /** Method Override */
   @override
   Widget build(BuildContext context) {
+
+    var questionList= [
+      'What is your mother name?',
+      'What is your favorite pet\'s name?'
+    ];
     /** MatrialApp Widget */
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("First App"),
+          title: Text("New First App"),
         ),
         body: Column(
           children: [
             // Text
-            Text("Question 1: What is your name?"),
+            Text(questionList.elementAt(0)),
 
             //Buttons
-            ElevatedButton(onPressed: null, child: Text("Answer 1"),),
-            ElevatedButton(onPressed: null, child: Text("Answer 2"),),
-            ElevatedButton(onPressed: null, child: Text("Answer 3"),),
+            ElevatedButton(
+              onPressed: questionAnswered,
+              child: Text("Answer A"),
+            ),
+            ElevatedButton(
+              onPressed: questionAnswered,
+              child: Text("Answer B"),
+            ),
+            ElevatedButton(
+              onPressed: questionAnswered,
+              child: Text("Answer C"),
+            ),
           ],
         ),
       ),
