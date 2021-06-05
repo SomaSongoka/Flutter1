975807d4e817a070d001ac6c04f34285da4eb73e
Josh L. Minga <joshlminga@gmail.com>
2021-06-05 14:02:44 +0300

    Debugged

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,31 @@
-/**
+/*
  * IMPORT
  * 1: Material Theme
  */
 import 'package:flutter/material.dart';
 
-/**
+/*
  *  Main Class to run our App
  */
 void main() => runApp(MyApp());
 
-/**
+/*
  * MyClass
  * - Main class to build and load our Flutter Widgets
  */
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
   //Question Answered Method
   void questionAnswered() {
-    print('Question Answer');
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
   }
 
-  /** Method Override */
+  /// Method Override */
   @override
   Widget build(BuildContext context) {
-
-    var questionList= [
+    var questionList = [
       'What is your mother name?',
       'What is your favorite pet\'s name?'
     ];
@@ -36,7 +38,7 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             // Text
-            Text(questionList.elementAt(0)),
+            Text(questionList.elementAt(questionIndex)),
 
             //Buttons
             ElevatedButton(
