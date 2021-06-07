e60cef33c384216b7b0c1e8731c4936b6e72aa98
Josh L. Minga <joshlminga@gmail.com>
2021-06-07 23:05:41 +0300

    Containers

--- lib/questions/wquestion.dart
+++ lib/questions/wquestion.dart
@@ -7,9 +7,21 @@ class WQuestion extends StatelessWidget {
   WQuestion(String questionTxt) {
     this.questionTxt = questionTxt;
   }
-
+  //Use control + space to get list of options
   @override
   Widget build(BuildContext context) {
-    return Text('Question: ' + questionTxt);
+    return Container(
+      //Help to control width and layout sizing
+      width: double.infinity,
+      margin: EdgeInsets.only(left: 5, right: 5, top: 30, bottom: 10),
+      child: Text(
+        questionTxt,
+        style: TextStyle(
+          color: Colors.red,
+          fontSize: 27,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
